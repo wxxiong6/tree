63eeda359cf5e5f5273a31084a7ad39c4cbaadff
vip6111111 <601115211@qq.com>
2016-12-23 15:47:49 +0800

    修改参数错误

--- Tree.php
+++ Tree.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ +------------------------------------------------------------------------------
+ * Tree
+ +------------------------------------------------------------------------------
+ * @author    wxxiong@gmail.com
+ * @version   v1.1
+ +------------------------------------------------------------------------------
+ */
 class Tree
 {
     //主键
@@ -7,14 +15,18 @@ class Tree
     private static $parentId = 'parent_id';
     //子节点名称
     private static $child    = 'child';
-
-
     public static function setConfig($primary = '', $parentId = '', $child = ''){
         if(!empty($primary)) self::$primary  = $primary;
-        if(!empty($prentId)) self::$parentId = $parentId;
+        if(!empty($parentId)) self::$parentId = $parentId;
         if(!empty($child))   self::$child    = $child;
     }
 
+    /**
+     * 生成Tree
+     * @param array $data
+     * @param number $index
+     * @return array
+     */
     public static  function  makeTree(&$data, $index = 0)
     {
         $childs = self::findChild($data, $index);
@@ -22,7 +34,6 @@ class Tree
         {
             return $childs;
         }
-
         foreach($childs as $k => &$v)
         {
             if(empty($data)) break;
@@ -36,10 +47,17 @@ class Tree
         return $childs;
     }
 
+    /**
+     * 查找子类
+     * @param array $data
+     * @param number $index
+     * @return array
+     */
     public static function findChild(&$data, $index)
     {
         $childs = [];
 		foreach ($data as $k => $v){
+            echo self::$parentId;exit;
 			if($v[self::$parentId] == $index){
 				$childs[]  = $v;
                 unset($v);
