d4279c4dda52b3fc8099f74c83cc7f708c7eb8ab
wxxiong6 <601115211@qq.com>
2018-05-07 08:58:39 +0800

    格式化代码
    格式化代码

--- Tree.php
+++ Tree.php
@@ -1,85 +1,100 @@
 <?php
 /**
- +------------------------------------------------------------------------------
+ *
  * Tree 构建tree状数据
- +------------------------------------------------------------------------------
+ *
  * @author    wxxiong@gmail.com
  * @version   v1.1
- +------------------------------------------------------------------------------
+ *
  */
+
 class Tree
 {
     /**
-     * 主键名称
+     * 主键名称.
+     *
      * @var string
      */
     private static $primary = 'id';
 
     /**
-     * 父键名称
+     * 父键名称.
+     *
      * @var string
      */
     private static $parentId = 'parent_id';
 
     /**
-     * 子节点名称
+     * 子节点名称.
+     *
      * @var string
      */
-    private static $child    = 'child';
+    private static $child = 'child';
 
     /**
-     * 修改主键名称、父键名称、子节点名称
+     * 修改主键名称、父键名称、子节点名称.
+     *
      * @param string $primary
      * @param string $parentId
      * @param string $child
      */
-    public static function setConfig($primary = '', $parentId = '', $child = ''){
-        if(!empty($primary))  self::$primary  = $primary;
-        if(!empty($parentId)) self::$parentId = $parentId;
-        if(!empty($child))    self::$child    = $child;
+    public static function setConfig($primary = '', $parentId = '', $child = '')
+    {
+        if (!empty($primary)) {
+            self::$primary = $primary;
+        }
+        if (!empty($parentId)) {
+            self::$parentId = $parentId;
+        }
+        if (!empty($child)) {
+            self::$child = $child;
+        }
     }
 
     /**
-     * 生成Tree
-     * @param array $data
-     * @param number $index
+     * 生成Tree.
+     *
+     * @param array  $data
+     * @param int $index
      * @return array
      */
-    public static  function  makeTree(&$data, $index = 0)
+    public static function makeTree(&$data, $index = 0)
     {
-        $childs = self::findChild($data, $index);
-        if(empty($childs))
-        {
-            return $childs;
+        $children = self::findChild($data, $index);
+        if (empty($children)) {
+            return $children;
         }
-        foreach($childs as $k => &$v)
-        {
-            if(empty($data)) break;
+        foreach ($children as $k => &$v) {
+            if (empty($data)) {
+                break;
+            }
             $child = self::makeTree($data, $v[self::$primary]);
-            if(!empty($child))
-            {
+            if (!empty($child)) {
                 $v[self::$child] = $child;
             }
         }
         unset($v);
-        return $childs;
+
+        return $children;
     }
 
     /**
-     * 查找子类
-     * @param array $data
+     * 查找子类.
+     *
+     * @param array  $data
      * @param number $index
      * @return array
      */
     public static function findChild(&$data, $index)
     {
-        $childs = [];
-		foreach ($data as $k => $v){
-			if($v[self::$parentId] == $index){
-				$childs[]  = $v;
+        $children = [];
+        foreach ($data as $k => $v) {
+            if ($v[self::$parentId] === $index) {
+                $children[] = $v;
                 unset($v);
-			}
-		}
-		return $childs;
+            }
+        }
+
+        return $children;
     }
 }
