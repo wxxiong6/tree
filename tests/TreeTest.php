0ec82affa53d3d7352217f9362e6fd1a129e3ef1
wxxiong6 <601115211@qq.com>
2020-07-08 08:23:59 +0800

    修改tests

--- tests/TreeTest.php
+++ tests/TreeTest.php
@@ -2,8 +2,10 @@
 /**
  * User: wxxiong6@gmail.com
  * Date: 2019/12/16
- * Time: 10:07
+ * Time: 10:07.
  */
+
+namespace wxxiong6\tree\tests;
 
 use PHPUnit\Framework\TestCase;
 use wxxiong6\tree\Tree;
@@ -12,12 +14,15 @@ class TreeTest extends TestCase
 {
     public function testMakeTree()
     {
-        global $data;
+        $data = require __DIR__.'/data.php';
         $tree = Tree::makeTree($data);
         $this->assertCount(1, $tree);
         $this->assertCount(2, $tree[0]['child']);
         $this->assertCount(4, $tree[0]['child'][0]);
         $this->assertCount(4, $tree[0]['child'][1]);
+        $this->assertEquals('北京', $tree[0]['child'][0]['city']);
+        $this->assertEquals('北京市', $tree[0]['child'][0]['child'][0]['city']);
+        $this->assertEquals('上海', $tree[0]['child'][1]['city']);
+        $this->assertEquals('上海市', $tree[0]['child'][1]['child'][0]['city']);
     }
-
 }
--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * User: wxxiong6@gmail.com
- * Date: 2019/12/16
- * Time: 11:32
- */
-require_once dirname(dirname(dirname(__DIR__))) . "/bootstrap.php";
-$data = require_once __DIR__ . "/data.php";
